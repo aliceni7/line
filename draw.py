2cf4a04d7caf16d94bf4a4b47316dfca27065d0e
Alice <ani00@stuy.edu>
2020-02-10 20:47:14 -0500

    added all

--- draw.py
+++ draw.py
@@ -5,38 +5,53 @@ def draw_line( x0, y0, x1, y1, screen, color ):
         B = -1 * (x1 - x0)
         if (B == 0):
             while (y0 <= y1):
-                    plot(screen, color, x0, y0)
-                    y0 = y0 + 1
-        slope = -1 * (A / B)
-        # octant 1
-        if (slope > 0 and slope < 1):
-            d = 2 * A + B
+                plot(screen, color, int(x0), int(y0))
+                y0 = y0 + 1        
+        elif (A == 0):
             while (x0 <= x1):
-                plot(screen, color, x0, y0)
-                if (d > 0):
-                    y0 = y0 + 1
-                    d = d + 2 * B
+                plot(screen, color, int(x0), int(y0))
                 x0 = x0 + 1
-                d = d + 2 * A
-        # octant 2
-        if (slope > 1):
-            d = A + 2 * B
-            while (y0 <= y1):
-                plot(screen,color, x0, y0)
-                if (d < 0):
-                    x0 = x0 + 1
-                    d = d + 2 * A
-                y0 = y0 + 1
-                d = d + 2 * B
-        # octant 7
-        if (slope > -1 and slope < 0):
-            d = B - 2 * A
-            while ( y0 <= y1 ):
-                plot(screen, color, x0, y0)
-                if (d < 0):
-                    x0 = x0 + 1
-                    d = d - 2 * A
-                y0 = y0 + 1
-                d = d + 2 * B
+        else:
+                slope = -1 * (A / B)
+                # octant 1
+                if (slope > 0 and slope <= 1):
+                        d = 2 * A + B
+                        while (x0 <= x1):
+                                plot(screen, color, x0, y0)
+                                if (d > 0):
+                                        y0 = y0 + 1
+                                        d = d + 2 * B
+                                x0 = x0 + 1
+                                d = d + 2 * A
+                # octant 2
+                if (slope > 1):
+                        d = A + 2 * B
+                        while (y0 <= y1):
+                                plot(screen,color, x0, y0)
+                                if (d < 0):
+                                        x0 = x0 + 1
+                                        d = d + 2 * A
+                                y0 = y0 + 1
+                                d = d + 2 * B
+                # octant 3
+                if (slope > -1 and slope < 0):
+                        d = 2 * A - B
+                        while ( x0 <= x1 ):
+                                plot(screen, color, x0, y0)
+                                if (d > 0):
+                                        y0 = y0 - 1
+                                        d = d + 2 * B
+                                x0 = x0 + 1
+                                d = d - 2 * A
+                # octant 4
+                if (slope <= -1):
+                        d = A - 2 * B
+                        while ( y0 >= y1 ):
+                                plot(screen, color, x0, y0)
+                                if (d < 0):
+                                        x0 = x0 + 1
+                                        d = d - 2 * A
+                                y0 = y0 - 1
+                                d = d + 2 * B
                 
             
